76ce05d2f2155a1cf8d29e130a9f8748f55dc90b
Daniel Estrada <isc.daniel.estrada@gmail.com>
2020-02-18 13:19:46 -0600

    ...

---
+++ src/data_table.py
@@ -0,0 +1,17 @@
+class DataTable:
+
+    def __init__(self, headers, dataset):
+        self.__dataset = dataset
+        pass
+
+    def __create(self):
+        
+        column_widths = []
+        for i in range(len(self.__dataset[0].keys())):
+            pass
+
+        index = 0
+        for key in self.__dataset[0].keys():
+            for i in range(len(self._dataset)):
+
+                pass
